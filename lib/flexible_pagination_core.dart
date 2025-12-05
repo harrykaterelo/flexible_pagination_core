536293996778815d7e61df1d89317f7234ce0da7
haris <katerelosharry@gmail.com>
2025-12-05 15:20:26 +0200

    added exports to main file

--- lib/flexible_pagination_core.dart
+++ lib/flexible_pagination_core.dart
@@ -1,5 +1,5 @@
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/pagination_usecase.dart';
+export 'src/pagination_options.dart';
+export 'src/pagination_result.dart';
+
